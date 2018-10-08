0aa47953c275bffd8da9c60c8563dbaaf376162d
Cedric David <cedric.david@btopenworld.com>
2018-10-08 14:56:10 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-// Code Goes Here
+import React from 'react';
+import PropTypes from 'prop-types';
+class Order extends React.Component {
+
+  render() {
+    return (
+      <div className="order">
+        <ul>
+          <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
+          <li>{this.props.size}</li>
+          <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
+          <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+
+
+Order.propTypes = {
+
+  cone: PropTypes.bool,
+  size: PropTypes.string,
+  scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+  orderInfo: PropTypes.shape({
+    customerName: PropTypes.string.isRequired,
+    orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
+  }).isRequired
+};
