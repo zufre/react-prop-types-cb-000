2f85d05f710d348255283551b742d1555aa9c407
Cedric David <cedric.david@btopenworld.com>
2018-10-08 14:51:29 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
- 
 class Order extends React.Component {
  
   render() {
-    // ...
+    return (
+      <div className="order">
+        <ul>
+          <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
+          <li>{this.props.size}</li>
+          <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
+          <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
+        </ul>
+      </div>
+    );
   }
 }
- 
-Order.defaultProps = {
-  cone: true,
-  size: 'regular'
-};
- 
-Order.propTypes = {};
+
+
 
 Order.propTypes = {
 
